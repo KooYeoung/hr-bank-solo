c5bccf80a3d37e11c2a438d9f1691b20f8473f8d
rndud <rndud2208@gmail.com>
2026-06-23 10:00:59 +0900

    정렬 및 포맷팅 처리.

--- src/main/java/com/kooyeoung/hrbank/entity/BackupInfo.java
+++ src/main/java/com/kooyeoung/hrbank/entity/BackupInfo.java
@@ -19,45 +19,49 @@ public class BackupInfo {
 
     @Getter
     private String worker;
+
     @Getter
     private LocalDateTime startedAt;
+
     @Getter
     private LocalDateTime endedAt;
+
     @Getter
     @Enumerated(EnumType.STRING)
     private BackupStatus status;
+
     @ManyToOne(fetch = FetchType.LAZY)
     private FileInfo backupFile;
 
-    public static BackupInfo skippedBackupInfo(String worker, LocalDateTime now){
-        return new BackupInfo(worker, now,now, BackupStatus.SKIPPED);
+    public static BackupInfo skippedBackupInfo(String worker, LocalDateTime now) {
+        return new BackupInfo(worker, now, now, BackupStatus.SKIPPED);
     }
 
-    public static BackupInfo inProgressBackupInfo(String worker, LocalDateTime now){
-        return new BackupInfo(worker, now,null, BackupStatus.IN_PROGRESS);
+    public static BackupInfo inProgressBackupInfo(String worker, LocalDateTime now) {
+        return new BackupInfo(worker, now, null, BackupStatus.IN_PROGRESS);
     }
 
-    private BackupInfo(String worker, LocalDateTime startedAt, LocalDateTime endedAt, BackupStatus status){
+    private BackupInfo(String worker, LocalDateTime startedAt, LocalDateTime endedAt, BackupStatus status) {
         this.worker = worker;
         this.startedAt = startedAt;
         this.endedAt = endedAt;
         this.status = status;
     }
 
-    public void complete(FileInfo backupFile){
+    public void complete(FileInfo backupFile) {
         this.backupFile = backupFile;
         this.status = BackupStatus.COMPLETED;
         this.endedAt = LocalDateTime.now();
     }
 
-    public void fail(FileInfo logFile){
+    public void fail(FileInfo logFile) {
         this.backupFile = logFile;
         this.status = BackupStatus.FAILED;
         this.endedAt = LocalDateTime.now();
     }
 
-    public Long getBackupFileId(){
-        if(backupFile == null ) return null;
+    public Long getBackupFileId() {
+        if (backupFile == null) return null;
         return backupFile.getId();
     }
 }
